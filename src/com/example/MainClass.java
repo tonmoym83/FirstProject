820a214b945de9503511833f0024017a20589d45
tonmoym83 <contacttonmoy@gmail.com>
2025-11-19 22:23:51 +0000

    First commit

--- src/com/example/MainClass.java
+++ src/com/example/MainClass.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class MainClass {
 	public static void main(String[] args) {
-		System.out.println("Hello12345, Github!");
+		System.out.println("Hello5678, Github!");
 	}
 }
